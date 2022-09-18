6d23f09d11e5c9fcf2457b61b4ba5e05eeb25287
t-kiryu <k040505t@gmail.com>
2022-09-18 03:13:15 +0000

    ログイン、会員登録実装

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -4,18 +4,26 @@ class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
+  def destroy
+    super
+  end
+
+  # def after_sign_in_path_for(resource)
+  #   root_path
+  # end
+
+  # def after_sign_out_path_for(resource)
+  #   root_path
   # end
 
   # protected
