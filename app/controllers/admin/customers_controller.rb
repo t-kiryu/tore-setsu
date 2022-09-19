a39b5cdb07c263e425e9d7079c9d6b798391151c
t-kiryu <k040505t@gmail.com>
2022-09-19 15:17:07 +0000

    会員一覧・詳細・編集画面の作成/管理者

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,8 +10,18 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    render :show
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :email, :is_deleted)
   end
 end
