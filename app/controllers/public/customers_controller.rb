04ffa24620706da16d3a863246254fbb8cf42d7d
t-kiryu <k040505t@gmail.com>
2022-09-19 07:22:23 +0000

    会員編集画面の更新

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -20,4 +20,11 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal
   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :email, :is_deleted)
+  end
+
 end
