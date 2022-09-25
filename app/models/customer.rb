448e53ace81c8c495d976445edaf2ef6fee151c7
t-kiryu <k040505t@gmail.com>
2022-09-25 13:39:14 +0000

    氏名のコード簡略化

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,13 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts,  dependent: :destroy
+
+  def full_name
+    last_name + " " + first_name
+  end
+
+  def full_name_kana
+    last_name_kana + " " + first_name_kana
+  end
+
 end
