33e1b6dbf4301bebdc59b6f64a21beeaba408fa9
t-kiryu <k040505t@gmail.com>
2022-10-04 03:40:21 +0000

    コメント機能更新

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -2,7 +2,11 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_customer!
 
   def create
-
+    post = Post.find(params[:post_id])
+    comment = current_customer.post_comment.new(post_comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(past)
   end
 
   def destroy
@@ -12,6 +16,6 @@ class Public::PostCommentsController < ApplicationController
   private
 
   def post_comment_parmas
-
+    params.require(:post).permit(:commnet)
   end
 end
