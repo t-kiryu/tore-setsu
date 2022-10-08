e07ea09235676fcaebbb72d679fc685fab169088
t-kiryu <k040505t@gmail.com>
2022-10-08 15:22:50 +0000

    コメント管理カラム作成

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -10,7 +10,8 @@ class Public::PostCommentsController < ApplicationController
   end
 
   def destroy
-
+    PostComment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_03_031546) do
+ActiveRecord::Schema.define(version: 2022_10_08_150511) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2022_10_03_031546) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "show_comment", default: true
   end
 
   create_table "posts", force: :cascade do |t|
