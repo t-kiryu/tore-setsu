964c71766fafec38a0e82030558cdc9961b83039
t-kiryu <k040505t@gmail.com>
2022-10-16 05:28:52 +0000

    ログイン、新規登録後の遷移先変更

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -39,7 +39,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_sign_up_path_for(resource)
-    root_path
+    posts_path
   end
 
   def after_sign_out_path_for(resource)
