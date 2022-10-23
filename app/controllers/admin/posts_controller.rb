94c08a64cf7de645a38728bd3ed839a37e143f5a
t-kiryu <k040505t@gmail.com>
2022-10-23 12:54:21 +0000

    ページネーション設定

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,8 +2,8 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @posts = Post.all
-    @posts = Post.all.order(created_at: :desc)
+    #@posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   def show
