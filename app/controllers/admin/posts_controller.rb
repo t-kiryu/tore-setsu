03544d3c0c5d1eab2ddc702b6a09be57a071a6c5
t-kiryu <k040505t@gmail.com>
2022-10-29 06:57:35 +0000

    コメント機能の更新

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -8,25 +8,10 @@ class Admin::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @post_comment = PostComment.find(params[:id])
   end
 
   def edit
     @post = Post.find(params[:id])
-  end
-
-  def update
-  #  @post = Post.find(params[:id])
-  #  @post.update(post_params)
-  #  redirect_to
-  end
-
-  def delete
-  end
-
-  private
-
-  def post_params
   end
 
 end
