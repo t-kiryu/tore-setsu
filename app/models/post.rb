fa3b107cf2ab2bf33415f0f4e40cfad8c6cafd53
t-kiryu <k040505t@gmail.com>
2022-10-30 07:02:39 +0000

    画像有無のコメント記入

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,8 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1, maximum: 30 }
   validates :introduction, presence: true, length: { minimum: 1, maximum: 255 }
 
+
+  # 画像添付なし=>no_image.jpg, 添付あり=>image
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
