664ddffff11c112a6c8b1ad3602dbfda53804768
t-kiryu <k040505t@gmail.com>
2022-10-30 12:38:03 +0000

    エラーメッセージ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,8 +15,11 @@ class Admin::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(params[:id])
-    @customer.update(customer_params)
-    render :show
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
   end
 
   def bookmark
