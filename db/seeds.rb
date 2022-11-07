8e2a1c2be2e9b3f926feae1c6ac0b8a0631a17bd
t-kiryu <k040505t@gmail.com>
2022-11-07 14:25:51 +0000

    seedファイル更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,27 +14,18 @@ Admin.create!(
 #   [
 #      { name: '20代' },
 #      { name: '30代' },
-#      { name: '40代' },
-#      { name: '胸筋' },
 #      { name: '腹筋' },
 #      { name: '背筋' },
-#      { name: 'マラソン' },
 #      { name: 'ジョギング' },
 #      { name: 'ピラティス' },
-#      { name: '運動不足' },
 #   ]
 #)
 #Customer.create!(
 #   [
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
-#      { last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', user_name: '', email: '', password: '' },
+#      { last_name: '令和', first_name: '道子', last_name_kana: 'レイワ', first_name_kana: 'ミチコ', user_name: 'みちこ', email: 'michiko@example.com', password: '111111' },
+#      { last_name: '平成', first_name: '太郎', last_name_kana: 'ヘイセイ', first_name_kana: 'タロウ', user_name: 'たろう', email: 'taro@example.com', password: '222222' },
+#      { last_name: '昭和', first_name: '一郎', last_name_kana: 'ショウワ', first_name_kana: 'イチロウ', user_name: 'いちろう', email: 'ichiro@example.com', password: '333333' },
+#      { last_name: '大正', first_name: '二郎', last_name_kana: 'タイショウ', first_name_kana: 'ジロウ', user_name: 'じろう', email: 'ziro@example.com', password: '444444' },
+#      { last_name: '明治', first_name: '三郎', last_name_kana: 'メイジ', first_name_kana: 'サブロウ', user_name: 'さぶろう', email: 'saburo@example.com', password: '555555' },
 #   ]
 #)
