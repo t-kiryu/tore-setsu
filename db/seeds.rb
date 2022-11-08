239318f912cd6e60f0c6aae2f1474ff0baa1e6cb
t-kiryu <k040505t@gmail.com>
2022-11-08 11:30:40 +0000

    seedファイル内容更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create!(
-   email:    'kanri@example.com',
-   password: 'xxxxxx',
+   email:    'kanri1@example.com',
+   password: '000000',
 )
-#Tag.create!(
-#   [
-#      { name: '20代' },
-#      { name: '30代' },
-#      { name: '腹筋' },
-#      { name: '背筋' },
-#      { name: 'ジョギング' },
-#      { name: 'ピラティス' },
-#   ]
-#)
-#Customer.create!(
-#   [
-#      { last_name: '令和', first_name: '道子', last_name_kana: 'レイワ', first_name_kana: 'ミチコ', user_name: 'みちこ', email: 'michiko@example.com', password: '111111' },
-#      { last_name: '平成', first_name: '太郎', last_name_kana: 'ヘイセイ', first_name_kana: 'タロウ', user_name: 'たろう', email: 'taro@example.com', password: '222222' },
-#      { last_name: '昭和', first_name: '一郎', last_name_kana: 'ショウワ', first_name_kana: 'イチロウ', user_name: 'いちろう', email: 'ichiro@example.com', password: '333333' },
-#      { last_name: '大正', first_name: '二郎', last_name_kana: 'タイショウ', first_name_kana: 'ジロウ', user_name: 'じろう', email: 'ziro@example.com', password: '444444' },
-#      { last_name: '明治', first_name: '三郎', last_name_kana: 'メイジ', first_name_kana: 'サブロウ', user_name: 'さぶろう', email: 'saburo@example.com', password: '555555' },
-#   ]
-#)
+
+customers = Customer.create!(
+   [
+      { last_name: '令和', first_name: '道子', last_name_kana: 'レイワ', first_name_kana: 'ミチコ', user_name: 'みちこ', email: 'michiko@example.com', password: '111111' },
+      { last_name: '平成', first_name: '太郎', last_name_kana: 'ヘイセイ', first_name_kana: 'タロウ', user_name: 'たろう', email: 'taro@example.com', password: '222222' },
+      { last_name: '昭和', first_name: '一郎', last_name_kana: 'ショウワ', first_name_kana: 'イチロウ', user_name: 'いちろう', email: 'ichiro@example.com', password: '333333' },
+      { last_name: '大正', first_name: '二郎', last_name_kana: 'タイショウ', first_name_kana: 'ジロウ', user_name: 'じろう', email: 'ziro@example.com', password: '444444' },
+      { last_name: '明治', first_name: '三郎', last_name_kana: 'メイジ', first_name_kana: 'サブロウ', user_name: 'さぶろう', email: 'saburo@example.com', password: '555555' },
+   ]
+)
+
+tags = ["20代", "30代", "腹筋", "背筋", "ジョギング","ピラティス"]
+tags.length.times do |i|
+ Tag.create!(
+    name: tags[i]
+)
+end
+
+customers.length.times do |i|
+   Post.create!(
+      title: '肥満解消',
+      introduction: '運動不足解消に手軽なものを探しています',
+      customer_id: i+1
+   )
+end
+
+posts = Post.all
+posts.length.times do |i|
+   TagList.create!(
+      post_id: i+1,
+      tag_id: i+1
+   )
+end
